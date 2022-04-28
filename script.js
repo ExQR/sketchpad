af3a9c711f949c8fb9a4f7d688e78114374c7b99
ExQRFx <exqr.fx@gmail.com>
2022-04-28 20:07:39 +0500

    function for generating grid

--- script.js
+++ script.js
@@ -4,18 +4,25 @@ rangeControl.oninput = function() {
 }
 function checkValue() {
     let val = document.getElementById('checks').value;
-    document.getElementById('rangeOutput').innerHTML = val;
+    document.getElementById('rangeOutput').innerHTML = `${val}x${val}`;
     return val;
 }
 
 function gridGen(val) {
-    const gridContainer = document.querySelector('.container');
+    const gridContainer = document.querySelector('.container'); 
     clearGrid();
-    for (let i=0; i<val; i++) {
+    for (let i=0; i<val*val; i++) {
         const square = document.createElement('div');
         square.classList.add('pixel');
         gridContainer.appendChild(square);
     }
+    let pixelSize = 512 / val;
+    gridContainer.style.gridTemplateColumns = `repeat(${val}, ${pixelSize}px)`;
+    const gridPixel = document.querySelectorAll('.pixel');
+    gridPixel.forEach((gridPixel) => {
+        gridPixel.style.width = `${pixelSize}px`;
+        gridPixel.style.height = `${pixelSize}px`
+        });
 }
 
 function clearGrid() {
