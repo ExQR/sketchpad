4e05abac5aaaf4e52cbb14963c9238a133c834d4
ExQRFx <exqr.fx@gmail.com>
2022-04-28 15:51:17 +0500

    functions for grid generation

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ rangeControl.oninput = function() {
 function checkValue() {
     let val = document.getElementById('checks').value;
     document.getElementById('rangeOutput').innerHTML = val;
+    return val;
 }
+
+function gridGen(val) {
+    const gridContainer = document.querySelector('.container');
+    clearGrid();
+    for (let i=0; i<val; i++) {
+        const square = document.createElement('div');
+        square.classList.add('pixel');
+        gridContainer.appendChild(square);
+    }
+}
+
+function clearGrid() {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(n => n.remove());
+}
+
+const applyButton = document.querySelector('#apply');
+applyButton.addEventListener('click', () => {
+    let val = checkValue();
+    gridGen(val);
+})
